de1b762db85f30c046fab50c760eafeaecba8293
Alexander <alex030t@stud.kea.dk>
2021-09-01 10:56:39 +0200

    Created a process

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -1,11 +1,22 @@
 package demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
         ProcessSim ps = new ProcessSim();
-        ps.runSimulation(new Process());
+
+        List<State> list = new ArrayList<>();
+
+        list.add(new State("Communication",1,3,1,1.0));
+        list.add(new State("Planning",2,1,2,0.8));
+        list.add(new State("Modelling",1,3,2,0.8));
+        list.add(new State("Deployment",1,1,0,1));
+
+        ps.runSimulation(new Process("UP",5,list));
 
     }
 }
--- src/demo/Process.java
+++ src/demo/Process.java
@@ -7,4 +7,16 @@ public class Process {
     private String name;
     private int nrOfStates;
     private List<State> listOfStates;
+
+    public Process(String name,int nrOfStates,List<State> listOfStates){
+        this.name=name;
+        this.nrOfStates=nrOfStates;
+        this.listOfStates = listOfStates;
+    }
+
+
+    public int getNrOfStates(){
+        return this.nrOfStates;
+    }
+
 }
